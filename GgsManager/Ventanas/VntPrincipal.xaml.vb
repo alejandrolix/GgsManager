d9cef7b7f588a33f22e2cfbc1fa1ef0fef4f76cc
Alejandro Pons <alejandro.pons98@gmail.com>
2018-02-21 12:44:18 +0100

    Menú Creado

---
+++ GgsManager/Ventanas/VntPrincipal.xaml.vb
@@ -0,0 +1,37 @@
+﻿Class MainWindow
+    Private Sub Garajes_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub Clientes_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub Vehiculos_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub Usuarios_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub Plazas_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub Listados_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub Facturas_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub Configuracion_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub CerrarSesion_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+End Class
